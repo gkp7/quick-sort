4de96b93dfe152a57b46e9f09aca9047f23c9715
gkp7 <42278374+gkp7@users.noreply.github.com>
2018-10-15 06:59:57 -0400

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,47 @@
+def quicksort(alist):
+        quicksorthelper(alist,0,len(alist)-1)
+        
+
+def quicksorthelper(alist,first,last):
+        if first<last:
+        
+                splitpoint=partition(alist,first,last)
+                
+                quicksorthelper(alist,first,splitpoint-1)
+                quicksorthelper(alist,splitpoint+1,last)
+                
+def partition(alist,first,last):
+        pivotvalue=alist[first]
+        
+        leftmark=first+1
+        rightmark=last
+        
+        done=False
+        
+        while not done:
+                while leftmark<=rightmark and alist[leftmark]<=pivotvalue:
+                        leftmark=leftmark+1
+                       
+                while alist[rightmark]>=pivotvalue and rightmark>= leftmark:
+                        rightmark=rightmark-1
+                
+                if rightmark < leftmark:
+                     done=True
+                else:
+                    
+                    temp=alist[leftmark]
+                    alist[leftmark]=alist[rightmark]
+                    alist[rightmark]=temp        
+            
+        temp=alist[first]
+        alist[first]=alist[rightmark]
+        alist[rightmark]=temp
+            
+        return rightmark
+
+
+alist=[57,26,93,77,33,44,50,20]
+quicksort(alist)
+print alist
+
+        
